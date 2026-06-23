269ef7941ad61a64d39ca0821612d957e5e4be73
its.an.error.ssms.is.here <sooryarvind@gmail.com>
2026-06-23 12:39:32 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[] visited = new boolean[10];
+        for(int row = 0; row < 9; row++){
+            Arrays.fill(visited,false);
+            for(int i = 0; i < 9; i++){
+                if(board[row][i]=='.')continue;
+                if(visited[(board[row][i]) - '0']) return false;
+                visited[(board[row][i]) - '0'] = true;
+            }
+        }
+        for(int col = 0; col < 9; col++){
+            Arrays.fill(visited,false);
+            for(int i = 0; i < 9; i++){
+                if(board[i][col]=='.')continue;
+                if(visited[(board[i][col]) - '0']) return false;
+                visited[(board[i][col]) - '0'] = true;
+            }
+        }
+        for(int square = 0; square < 9; square++){
+           Arrays.fill(visited,false);
+           for(int i = 0; i < 3; i++){
+                for(int j = 0; j < 3; j++){
+                    int row = (square/3)*3+i;
+                    int col = (square%3)*3+j;
+                    if(board[row][col]=='.')continue;
+                    if(visited[(board[row][col]) - '0']) return false;
+                    visited[(board[row][col]) - '0'] = true;
+                }
+            }
+        }
+        return true;
+
+    }
+}
